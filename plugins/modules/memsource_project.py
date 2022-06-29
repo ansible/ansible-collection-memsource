ffb270fea4a37b31b18f0990b3a15add5ef537c6
Aditya Mulik <aditya.mulik@gmail.com>
2022-06-29 06:37:00 -0400

    Add roles for generic translations support
    * project_uid has been kept as an optional var
    * removed root user from each roles tests
    * The namespace has been updated to ansible, initially it was community. The reason to update to ansible was, as we upload the collection to galaxy, the namespace expected is as per the GitHub Username
    * All Documentations has been corrected & updated
    * A extra vars template file has been added which can be used to execute each role
    * Python-memsource package has been updated, similarly it has been included in requirements.txt file
    * Post translation role is performed using the project_name retrieved from the string passed and the recent most project is retrieved from memsource.
    * removed root user from each roles tests
    * The namespace has been updated to ansible, initially it was community. The reason to update to ansible was, as we upload the collection to galaxy, the namespace expected is as per the GitHub Username
    * All Documentations has been corrected &updated
    * A extra vars template file has been added which can be used to execute each role
    * Python-memsource package has been updated, similarly it has been included in requirements.txt file
    * Post translation role is performed using the project_name retrieved from the string passed and the recent most project is retrieved from memsource.
    * Removed upstream/downstream repo and renamed to repo_url
    * Fixed assertions and documentation
    * fixes in pre_translation
    * updated extra_vars and made available global_vars
    * removed trailing spaces
    * removed references of repo_url and fork_repo_url
    * Added rebase pull from main url to forked repository
    
    added global vars
    
    removed email references
    
    added fork_url_repo for post_translation and updated docs
    
    fixed lint issues
    
    renamed namespace in job_info module

--- plugins/modules/memsource_project.py
+++ plugins/modules/memsource_project.py
@@ -35,23 +35,23 @@ options:
       - Whether to purge the content of the project on delete
     type: bool
 extends_documentation_fragment:
-- community.memsource.memsource
+- ansible.memsource.memsource
 
 requirements: [memsource]
 """
 
 EXAMPLES = """
 - name: Create project from template id
-  community.memsource.memsource_project:
+  ansible.memsource.memsource_project:
     name: My Project
     template_id: 12345
 
 - name: Retrieve project information
-  community.memsource.memsource_project:
+  ansible.memsource.memsource_project:
     uid: uid
 
 - name: Delete project
-  community.memsource.memsource_project:
+  ansible.memsource.memsource_project:
     uid: uid
     state: absent
 """
@@ -65,7 +65,7 @@ project:
 """
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.community.memsource.plugins.module_utils.memsource import (
+from ansible_collections.ansible.memsource.plugins.module_utils.memsource import (
     get_action,
     get_default_argspec,
     get_memsource_client,
